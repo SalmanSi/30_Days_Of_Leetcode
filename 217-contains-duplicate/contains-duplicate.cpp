f20a978772e68c8295a4f08609f4afce778503c6
SalmanSi <114280969+SalmanSi@users.noreply.github.com>
2024-03-17 14:52:18 +0500

    Time: 121 ms (19.12%) | Memory: 73.1 MB (43.67%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,11 +3,11 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         map<int,int> dupl;
         int len=nums.size();
-        for(int i=0;i<len;i++){
-            if(dupl[nums[i]]==1){
+        for(const auto& num:nums){
+            if(dupl[num]==1){
                 return true;
             }
-            dupl[nums[i]]=1;
+            dupl[num]=1;
         }
 
     return false;
