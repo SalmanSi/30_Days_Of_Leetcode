b26ce0eba36672085c31cedd28206230c7f1470c
SalmanSi <114280969+SalmanSi@users.noreply.github.com>
2024-07-04 09:39:38 +0500

    Time: 744 ms (91.65%) | Memory: 56.3 MB (67.77%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
@@ -5,20 +5,37 @@
 #         self.next = next
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        sums=[]
-        temp=0
-        current=head.next
-        while current:
-            if current.val == 0:
-                sums.append(temp)
-                temp=0
+        next_ptr=head
+        current_ptr=head
+        sum=0
+        while next_ptr:
+            if next_ptr.val == 0:
+                current_ptr.val=sum
+                # prevent overflow//null
+                current_ptr.next=next_ptr.next
+                current_ptr=current_ptr.next
+                next_ptr=current_ptr
+                sum=0
             else:
-                temp+=current.val
-            current=current.next
-        head=ListNode(sums[0])
-        current = head  
+                sum+=next_ptr.val
+                next_ptr=next_ptr.next
+        return head.next
 
-        for num in sums[1:]:
-            current.next=ListNode(num)
-            current=current.next
-        return head
+
+        # sums=[]
+        # temp=0
+        # current=head.next
+        # while current:
+        #     if current.val == 0:
+        #         sums.append(temp)
+        #         temp=0
+        #     else:
+        #         temp+=current.val
+        #     current=current.next
+        # head=ListNode(sums[0])
+        # current = head  
+
+        # for num in sums[1:]:
+        #     current.next=ListNode(num)
+        #     current=current.next
+        # return head
