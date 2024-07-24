63bf23ef22a46062772007134b1575c04e07803e
SalmanSi <114280969+SalmanSi@users.noreply.github.com>
2024-07-24 10:12:19 +0500

    Time: 936 ms (82.06%) | Memory: 26.7 MB (18.48%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -10,17 +10,36 @@ class Solution:
             digit_count*=10
         return mapped_num
     def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
-        mapped_nums=[]
+        # Using a lambda function to sort based on values of dict
+        # used the index as key and mapped num as values and sorted
+        # retrieved the keys from the sorted dict
+        # they were the sorted indices
+
+        # mapped_nums=[]
+        # for num in nums:
+        #     mapped_nums.append(self.getMapped(mapping,num))
+        # my_dict={}
+
+        # for i,num in enumerate(mapped_nums):
+        #     my_dict[i]=num
+        # my_dict=dict(sorted(my_dict.items(), key=lambda item:item[1]))
+
+        # indices=list(my_dict.keys())
+        # mapped_nums=[]
+        # for index in indices:
+        #     mapped_nums.append(nums[index])
+        # return mapped_nums
+
+        # using defaultdict with lists as values to store multiple values  under the same key
+
+        dic=defaultdict(list)
         for num in nums:
-            mapped_nums.append(self.getMapped(mapping,num))
-        my_dict={}
+            mapped_num=self.getMapped(mapping,num)
+            dic[mapped_num].append(num)
+        sorted_mapped_values=sorted(dic.keys())
 
-        for i,num in enumerate(mapped_nums):
-            my_dict[i]=num
-        my_dict=dict(sorted(my_dict.items(), key=lambda item:item[1]))
+        ans=[]
+        for val in sorted_mapped_values:
+            ans+=dic[val]
 
-        indices=list(my_dict.keys())
-        mapped_nums=[]
-        for index in indices:
-            mapped_nums.append(nums[index])
-        return mapped_nums
+        return ans
