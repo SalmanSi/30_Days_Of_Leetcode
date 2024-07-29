89a04e4f8e431f05f8a12b2576287ba06b31a44b
SalmanSi <114280969+SalmanSi@users.noreply.github.com>
2024-07-29 10:03:26 +0500

    Time: 493 ms (55.38%) | Memory: 16.7 MB (35.59%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.py
+++ 1511-count-number-of-teams/count-number-of-teams.py
@@ -1,32 +1,50 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
+        # length=len(rating)
+        # right_smaller_count=[0]*length
+        # left_smaller_count=[0]*length
+        # right_larger_count=[0]*length
+        # left_larger_count=[0]*length
+
+        # for i in range(0,length):
+        #     for j in range(i,length):# right count
+        #         if rating[j]>rating[i]:
+        #             right_larger_count[i]+=1
+        #         elif rating[j]<rating[i]:
+        #             right_smaller_count[i]+=1
+
+        #     for j in range(0,i):# left count
+        #         if rating[j]>rating[i]:
+        #             left_larger_count[i]+=1
+        #         elif rating[j]<rating[i]:
+        #             left_smaller_count[i]+=1                  
+
+        # ans=0
+        # ans+=sum(a*b for a,b in zip(left_smaller_count,right_larger_count))
+        # ans+=sum(a*b for a,b in zip(left_larger_count,right_smaller_count))
+        # # ans=numpy.dot(left_smaller_count,right_larger_count)+np.dot(left_larger_count,right_smaller_count)
+        # return ans
+
+        #more efficient instead of taking dot product
         length=len(rating)
-        right_smaller_count=[0]*length
-        left_smaller_count=[0]*length
-        right_larger_count=[0]*length
-        left_larger_count=[0]*length
-
+        res=0
         for i in range(0,length):
-            for j in range(i,length):
-                # if j>i:#right larger_count
-                if rating[j]>rating[i]:
-                    right_larger_count[i]+=1
-                elif rating[j]<rating[i]:
-                    right_smaller_count[i]+=1
-                # elif j<i:#left larger count
-                #     if rating[j]>rating[i]:
-                #         left_larger_count[i]+=1
-                #     elif rating[j]<rating[i]:
-                #         left_smaller_count[i]+=1
-            for j in range(0,i):
-                if rating[j]>rating[i]:
-                    left_larger_count[i]+=1
-                elif rating[j]<rating[i]:
-                    left_smaller_count[i]+=1                  
+                
+            right_smaller_count,left_smaller_count=0,0
+            right_larger_count,left_larger_count=0,0
 
-        ans=0
-        ans+=sum(a*b for a,b in zip(left_smaller_count,right_larger_count))
-        ans+=sum(a*b for a,b in zip(left_larger_count,right_smaller_count))
-        # ans=numpy.dot(left_smaller_count,right_larger_count)+np.dot(left_larger_count,right_smaller_count)
-        return ans
+            for j in range(i,length):# right count
+                if rating[j]>rating[i]:
+                    right_larger_count+=1
+                elif rating[j]<rating[i]:
+                    right_smaller_count+=1
 
+            for j in range(0,i):# left count
+                if rating[j]>rating[i]:
+                    left_larger_count+=1
+                elif rating[j]<rating[i]:
+                    left_smaller_count+=1  
+
+            res+=left_smaller_count*right_larger_count+left_larger_count*right_smaller_count
+
+        return res
