51cb442813bdd4d013ac5053020e03f3f807a746
SalmanSi <114280969+SalmanSi@users.noreply.github.com>
2024-08-04 01:06:18 +0500

    Time: 11 ms (43.55%) | Memory: 17.5 MB (43.00%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        map<int,int> freq_count;
-        for(auto num : target){
-            if(freq_count.find(num)==freq_count.end()){
-                freq_count[num]=1;
-            }else{
-                freq_count[num]++;
-            }
-        }
+        // map<int,int> freq_count;
+        // for(auto num : target){
+        //     if(freq_count.find(num)==freq_count.end()){
+        //         freq_count[num]=1;
+        //     }else{
+        //         freq_count[num]++;
+        //     }
+        // }
 
-        for (auto num:arr){
-            if(freq_count.find(num)==freq_count.end()){
-                return false;
-            }
-            else if (freq_count[num]>0){
-                freq_count[num]--;
-            }else{
-                return false;
-            }
-        }
+        // for (auto num:arr){
+        //     if(freq_count.find(num)==freq_count.end()){
+        //         return false;
+        //     }
+        //     else if (freq_count[num]>0){
+        //         freq_count[num]--;
+        //     }else{
+        //         return false;
+        //     }
+        // }
 
-        for(const auto &pair:freq_count){
-            if(pair.second>0 || pair.second<0){
-                return false;
-            }
-        }
-        return true;
+        // for(const auto &pair:freq_count){
+        //     if(pair.second>0 || pair.second<0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+        return arr==target;
     }
 };
