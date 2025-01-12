16172a17c6cfdc47a21ecbe83a09023ab23f2f64
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-01-12 21:02:01 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (60.37%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -8,23 +8,13 @@ class Solution:
             else:
                 prefixSum0.append(prefixSum0[-1])
         print(prefixSum0)
-
         totalZeros=prefixSum0[-1]
-
         max_=0
         scores=[]
         for idx,z in enumerate(prefixSum0[1:-1]):
             ones=length-(idx+1)-(totalZeros-z)
-            print(f"idx:{idx}")
-            print(f"ones:{ones}")
-            print(f"zeros:{z}")
             score=z+ones
-            print(f"score:{score}")
-            scores.append(score)
             max_=max(score,max_)
-            print("-------")
-
-        print(scores)
         return max_
 
 
