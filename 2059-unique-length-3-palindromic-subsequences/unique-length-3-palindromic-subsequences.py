d3a0bf8e656dc34ea5233b960d2c9812f4b1f6f4
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-01-19 01:42:33 +0500

    Time: 779 ms (33.69%) | Memory: 18.3 MB (62.79%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,19 +1,21 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        myset=set()
         left_char_set=set()
         r=len(s)-1
         l=0
+        ans=0
         for L in range(len(s)-2):
             if s[L] in left_char_set:
                 continue
             l=L
             r=len(s)-1
             while(r-l>1 and s[l]!=s[r]):
-                r-=1
-     
+                r-=1# come from the right to find the rightmost occurence of the char
+            
+            myset=set()
             for i in range(l+1,r):
-                myset.add(str(s[l]+s[i]+s[r]))
+                myset.add(s[i])
             left_char_set.add(s[L])
-        return len(myset)
+            ans+=len(myset)
+        return ans
         
