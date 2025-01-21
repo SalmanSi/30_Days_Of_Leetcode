caf626886a4a53d6ed6fa3e777f12f1619ea9c89
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-01-21 12:54:28 +0500

    Time: 8 ms (71.57%) | Memory: 18.3 MB (14.66%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,11 +1,38 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        ans=[0]*len(boxes)
+        ones=0
+        prefix_ones=[0]*len(boxes)
+        idx_sum=0
+        prefix_idx_sum=[0]*len(boxes)
 
         for i in range(len(boxes)):
-            for j in range(len(boxes)):
-                if i==j:
-                    continue
-                if boxes[j]=='1':
-                    ans[i]+=abs(i-j)
+            if boxes[i]=='1':
+                ones+=1
+                idx_sum+=i
+            prefix_ones[i]=ones
+            prefix_idx_sum[i]=idx_sum
+        ans=[0]*len(boxes)
+        for i in range(len(boxes)):
+            if i==0:
+                ans[i]=idx_sum
+                continue
+            elif i==len(boxes)-1 and i>0:
+                ans[i]=abs(prefix_idx_sum[i-1]-i*prefix_ones[i-1])
+                continue
+            left=abs(prefix_idx_sum[i-1]-i*prefix_ones[i-1])
+            right=(idx_sum-prefix_idx_sum[i])-i*(ones-prefix_ones[i])
+            ans[i]=left+right
         return ans
+
+
+
+        # very inefficient solution- O(n*n)
+        # ans=[0]*len(boxes)
+
+        # for i in range(len(boxes)):
+        #     for j in range(len(boxes)):
+        #         if i==j:
+        #             continue
+        #         if boxes[j]=='1':
+        #             ans[i]+=abs(i-j)
+        # return ans
