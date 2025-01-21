f44eebef376fc1c6fee9354e3785e6d5e7437ee2
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-01-21 19:58:52 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.11%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -1,12 +1,19 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        n=len(words)
-        words.sort(key=len)
+        words_joined=' '.join(words)
         ans=[]
-
-        for i in range(n):
-            for j in range(i+1,n):
-                if words[i] in words[j]:
-                    ans.append(words[i])
-                    break
+        for word in words:
+            if words_joined.count(word)>1:
+                ans.append(word)
         return ans
+
+        # n=len(words)
+        # words.sort(key=len)
+        # ans=[]
+
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         if words[i] in words[j]:
+        #             ans.append(words[i])
+        #             break
+        # return ans
