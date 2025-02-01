c96df5e87be8adac19589c429d6a32a0a8d50c23
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-02-01 20:15:13 +0500

    Time: 6 ms (37.60%) | Memory: 17.8 MB (66.53%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -41,7 +41,25 @@ class Solution:
         
 
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        root=self.recurse(l1,l2,None,0)
-        return root
+        # root=self.recurse(l1,l2,None,0)
+        # return root
+
+        dummyNode=ListNode(0)
+        curr=dummyNode
+        c=0
+
+        while l1  != None or l2  != None or c!=0:
+            l1Val=l1.val if l1 else 0
+            l2Val=l2.val if l2 else 0
+            sum=l1Val+l2Val+c
+            c=sum//10
+            node= ListNode(sum%10)
+            curr.next=node
+            curr=node
+            l1=l1.next if l1 else None
+            l2=l2.next if l2 else None
+        return dummyNode.next
+
+            
 
         
