429f82125398d40aec1ff4e604b8c2c453f4496d
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-02-01 17:24:04 +0500

    Time: 1 ms (35.36%) | Memory: 17.9 MB (22.90%) - LeetSync

--- 3429-special-array-i/special-array-i.py
+++ 3429-special-array-i/special-array-i.py
@@ -1,11 +1,7 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        if len(nums)==1:
-            return True
-        prev_even=(nums[0]%2==0)
-        for i in range(1,len(nums)):
-            curr_even=(nums[i]%2==0)
-            if not(prev_even ^ curr_even):
+
+        for i in range(len(nums)-1):
+            if ((nums[i]&1)^(nums[i+1]&1)==0):
                 return False
-            prev_even=curr_even
         return True
