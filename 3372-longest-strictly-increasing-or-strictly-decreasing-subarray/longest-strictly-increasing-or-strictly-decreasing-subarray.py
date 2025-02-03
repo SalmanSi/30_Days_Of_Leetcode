9d6d4951087adfdd05c88651c126028b497c052c
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-02-03 21:05:20 +0500

    Time: 1 ms (66.02%) | Memory: 17.8 MB (47.33%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -1,31 +1,44 @@
 class Solution:
     def longestMonotonicSubarray(self, nums: List[int]) -> int:
-        ans=0
-        curr=1
-        length=len(nums)
-        i=0
-        incr=False
+        incLen=decLen=maxLen=1
+        for i in range(len(nums)-1):
+            if nums[i+1]>nums[i]:
+                incLen+=1
+                decLen=1
+            elif nums[i+1]<nums[i]:
+                decLen+=1
+                incLen=1
+            else:
+                decLen=incLen=1
+            maxLen=max(decLen,incLen,maxLen)
+
+        return maxLen
+        # ans=0
+        # curr=1
+        # length=len(nums)
+        # i=0
+        # incr=False
         
-        while i<length-1:
-            if nums[i]==nums[i+1]:
-                ans=max(ans,curr)
-                curr=1
-            elif incr:
-                if nums[i]>nums[i+1]:
-                    ans=max(ans,curr)
-                    curr=1
-                    incr=False
-                    i-=1
-                else:
-                    curr+=1
-            elif not incr:
-                if nums[i]<nums[i+1]:
-                    ans=max(ans,curr)
-                    curr=1
-                    incr=True
-                    i-=1
-                else:
-                    curr+=1
-            i+=1
-        ans=max(ans,curr)
-        return ans
+        # while i<length-1:
+        #     if nums[i]==nums[i+1]:
+        #         ans=max(ans,curr)
+        #         curr=1
+        #     elif incr:
+        #         if nums[i]>nums[i+1]:
+        #             ans=max(ans,curr)
+        #             curr=1
+        #             incr=False
+        #             i-=1
+        #         else:
+        #             curr+=1
+        #     elif not incr:
+        #         if nums[i]<nums[i+1]:
+        #             ans=max(ans,curr)
+        #             curr=1
+        #             incr=True
+        #             i-=1
+        #         else:
+        #             curr+=1
+        #     i+=1
+        # ans=max(ans,curr)
+        # return ans
