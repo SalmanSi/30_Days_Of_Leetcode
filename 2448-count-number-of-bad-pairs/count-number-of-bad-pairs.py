c81a8bd1427059e1a9ee3a367ac99594b8bb7a36
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-02-09 12:46:16 +0500

    Time: 91 ms (42.38%) | Memory: 38.9 MB (59.29%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -7,11 +7,10 @@ class Solution:
             all_pairs+=i
             diff_count[i-nums[i]]+=1
         
-        print(diff_count)
-        print(all_pairs)
+
         good_pairs=0
         for diff,count in diff_count.items():
             if count>1:
                 good_pairs+=math.comb(count,2)
-        print(good_pairs)
+
         return all_pairs-good_pairs
