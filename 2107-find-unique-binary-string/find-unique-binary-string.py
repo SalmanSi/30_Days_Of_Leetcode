97029766a44116a868117b3895b7e0e9cb228250
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-02-20 19:36:46 +0500

    Time: 726 ms (8.60%) | Memory: 25.4 MB (9.90%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -0,0 +1,20 @@
+class Solution:
+
+
+    def getAllString(self,n:int,nums: set,myset:set, cur:str) -> None:
+        if len(cur)==n:
+            if cur not in nums:
+                myset.add(cur)
+            return 
+        self.getAllString(n,nums,myset,cur+'0')
+        self.getAllString(n,nums,myset,cur+'1')
+        return      
+
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        n=len(nums[0])
+        nums=set(nums)
+        myset=set()
+
+        self.getAllString(n,nums,myset,"")
+        for x in myset:
+            return x
