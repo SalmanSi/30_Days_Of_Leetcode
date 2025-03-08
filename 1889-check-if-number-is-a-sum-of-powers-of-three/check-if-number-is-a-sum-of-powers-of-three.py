75c8016e46366650f739517237045054be186b4b
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-03-09 02:11:14 +0500

    Time: 234 ms (11.43%) | Memory: 17.7 MB (86.30%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,9 +1,21 @@
 class Solution:
     
-
+    def helper(self,n,power):
+        if n==0:
+            return True
+        if 3**power>n:
+            return False
+        
+        keep=self.helper(n-(3**power),power+1)
+        skip=self.helper(n,power+1)
+        if keep or skip:
+            return True
+        return False
     def checkPowersOfThree(self, n: int) -> bool:
-        while (n>0):
-            if n%3>1:
-                return False
-            n=n//3
-        return True
+        # while (n>0):
+        #     if n%3>1:
+        #         return False
+        #     n=n//3
+        # return True
+
+        return self.helper(n,0)
