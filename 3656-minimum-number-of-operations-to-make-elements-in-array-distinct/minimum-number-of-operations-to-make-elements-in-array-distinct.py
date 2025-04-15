6d504f6b2d3f60aab82e154b3b54e336f36c121f
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-04-15 22:42:11 +0500

    Time: 1 ms (72.79%) | Memory: 17.8 MB (75.51%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,15 +1,24 @@
 class Solution:
-    def checkUnique(self,nums,start):
-        myset=set()
-        for num in nums[start:]:
-            if num in myset:
-                return False
-            myset.add(num)
-        return True 
+    # def checkUnique(self,nums,start):
+    #     myset=set()
+    #     for num in nums[start:]:
+    #         if num in myset:
+    #             return False
+    #         myset.add(num)
+    #     return True 
     def minimumOperations(self, nums: List[int]) -> int:
-        ans=0
-        for i in range(0,len(nums),3):
-            if self.checkUnique(nums,i):
-                return ans
-            ans+=1
-        return ans
+        # ans=0
+        # for i in range(0,len(nums),3):
+        #     if self.checkUnique(nums,i):
+        #         return ans
+        #     ans+=1
+        # return ans
+
+        # optimized solution
+        seen=[0]*101
+        for i in range(len(nums)-1,-1,-1):
+            if seen[nums[i]]:
+                return (i//3 +1)
+            else:
+                seen[nums[i]]=1
+        return 0
