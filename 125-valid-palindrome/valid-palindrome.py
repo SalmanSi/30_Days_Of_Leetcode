bf4b95af5731595b67d0ab2e1076c3733c47c33b
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-04-16 01:04:04 +0500

    Time: 15 ms (13.68%) | Memory: 23.3 MB (10.47%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,17 +1,27 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s=s.lower()
-        alphabets={'a','b','c','d','e','f'
-        ,'g','h','i','j','k','l','m','n','o','p','q' ,'r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'  }
-        mylist=[]
-        for ch in s:
-            if ch in alphabets:
-                mylist.append(ch)
+        # s=s.lower()
+        # alphabets={'a','b','c','d','e','f'
+        # ,'g','h','i','j','k','l','m','n','o','p','q' ,'r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'  }
+        # mylist=[]
+        # for ch in s:
+        #     if ch in alphabets:
+        #         mylist.append(ch)
         
-        size=len(mylist)
-        print(mylist)
-        for i in range(0,size//2):
-            if mylist[i]!=mylist[-i-1]:
-                print(i)
+        # size=len(mylist)
+        # print(mylist)
+        # for i in range(0,size//2):
+        #     if mylist[i]!=mylist[-i-1]:
+        #         print(i)
+        #         return False
+        # return True
+
+        s=''.join(c.lower() for c in s if c.isalnum() )
+        left=0
+        right=len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
                 return False
+            left+=1
+            right-=1
         return True
