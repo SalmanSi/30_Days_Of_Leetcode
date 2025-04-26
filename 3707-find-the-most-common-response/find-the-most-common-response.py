89e9573f75cbc9b72c1d4b698e9335304db83d25
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-04-27 01:11:25 +0500

    Time: 562 ms (20.95%) | Memory: 290.4 MB (5.01%) - LeetSync

--- 3707-find-the-most-common-response/find-the-most-common-response.py
+++ 3707-find-the-most-common-response/find-the-most-common-response.py
@@ -1,13 +1,21 @@
 class Solution:
     def findCommonResponse(self, responses: List[List[str]]) -> str:
-        mymap=defaultdict(int)
+        res_freq={}
         for resp in responses:
-            resp=list(set(resp))
-            for word in resp:
-                mymap[word]+=1
+            unq_resp = list(dict.fromkeys(resp))
+            for res in unq_resp:
+                res_freq[res]=res_freq.get(res,0)+1
+        res_freq=sorted(res_freq.items(),key=lambda x: (-x[1],x[0]))
+        return res_freq[0][0]
+
+        # mymap=defaultdict(int)
+        # for resp in responses:
+        #     resp=list(set(resp))
+        #     for word in resp:
+        #         mymap[word]+=1
                     
             
-        mymap=dict(sorted(mymap.items(),key=lambda x: x[0], reverse=False ))
-        mymap=dict(sorted(mymap.items(),key=lambda x: x[1], reverse=True ))
-        return (list(mymap.keys())[0])
+        # mymap=dict(sorted(mymap.items(),key=lambda x: x[0], reverse=False ))
+        # mymap=dict(sorted(mymap.items(),key=lambda x: x[1], reverse=True ))
+        # return (list(mymap.keys())[0])
  
