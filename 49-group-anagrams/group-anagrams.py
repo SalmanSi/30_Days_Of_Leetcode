cbeb2c2eb8af0d952946c3e471f1c11793577d76
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-05-06 18:13:38 +0500

    Time: 11 ms (82.95%) | Memory: 20.6 MB (76.52%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,14 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_list_map={}
+        sorted_list_map=defaultdict(list)
         for word in strs:
-            copy=str(sorted(word))
-            if copy not in sorted_list_map:
-                sorted_list_map[copy]=[]
+            copy="".join((sorted(word)))
             sorted_list_map[copy].append(word)
-        ans=[]
-        for k,v in sorted_list_map.items():
-            ans.append(v)
+        ans=list(sorted_list_map.values())
         return ans
 
 
