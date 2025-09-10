222b8f28217b009d67520c3b0923b5756c20b00e
Muhammad Salman Siddiq <114280969+SalmanSi@users.noreply.github.com>
2025-09-10 17:34:31 +0500

    Time: 10 ms (75.00%) | Memory: 18 MB (50.27%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        mymap=defaultdict(int)
+        smap=defaultdict(int)
+        tmap=defaultdict(int)
         for c in s:
-            mymap[c]+=1
-
+            smap[c]+=1
         for c in t:
-            if c not in mymap or mymap[c]==0:
+            tmap[c]+=1
+
+        if len(smap.items())!=len(tmap.items()):
+            return False
+        for k,v in smap.items():
+            if tmap[k]!=v:
                 return False
-            else:
-                mymap[c]-=1
-        return sum(list(mymap.values()))==0       
-        
+        return True
